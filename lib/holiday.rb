9869c50cc46aa266e687110561c0bdbfd6724d0f
Richard Loera <richexp9@gmx.com>
2018-01-16 20:51:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each { |season, holiday|
+    holiday.each { |items|
+      puts items
+    }
+  }
 
 end
 
