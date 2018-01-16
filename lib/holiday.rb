39577cdaca6558abb8a912c60a4f0ae39e239817
Richard Loera <richexp9@gmx.com>
2018-01-16 21:02:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidayList.each { |holiday, items|
       print "  #{holiday}:"
       items.each { |item|
-        puts item
+        print item
       }
     }
   }
